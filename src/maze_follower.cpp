474c021f5fb6862c158e9a1bafe6d5f03665bd5e
JohanJohansson <ras@ras-VirtualBox.(none)>
2014-11-10 15:21:59 +0100

    Makes the maze follower use the nodes turn_controller and wall_following_controller insted of have all the code itself

--- src/maze_follower.cpp
+++ src/maze_follower.cpp
@@ -1,10 +1,8 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/Twist.h"
-#include "ras_arduino_msgs/ADConverter.h"
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <ras_arduino_msgs/ADConverter.h>
+#include <wall_follower/MakeTurn.h>
   
-#include <sstream>
-
 int d1, d2, d3, d4, d5;
 
 class MazeController {
@@ -19,118 +17,16 @@ public:
         n = ros::NodeHandle();
         distance_sub = n.subscribe("/ir_sensor_cm", 1, &MazeController::MazeCallback, this);
         twist_pub = n.advertise<geometry_msgs::Twist>("/motor_controller/twist", 1);
+        turn_client = n.serviceClient<wall_follower::MakeTurn>("make_turn");
+        follow_client = n.serviceClient<wall_follower::MakeTurn>("follow_wall");
     }
 
-    void MazeCallback(const ras_arduino_msgs::ADConverter::ConstPtr &msg)
-     {
-       d1 = msg->ch1;
-       d2 = msg->ch2;
-       d3 = msg->ch3;
-       d4 = msg->ch4;
-       d5 = msg->ch5;
-       ROS_INFO("d1: [%d], d2: [%d], d3: [%d], d4: [%d], d5: [%d]", d1, d2, d3, d4, d5);
-     }
-
-    void TurnLeft()
-    {
-       ros::Rate loop_rate(10);
-
-       msg.linear.x = 0;
-       msg.linear.y = 0;
-       msg.linear.z = 0;
-       msg.angular.x = 0;
-       msg.angular.y = 0;
-       msg.angular.z = 0.314;
-       for (int i = 0; i < 30; i++) {
-           if (i < 10) {
-               msg.angular.z = 1.57;
-               publishMsg();
-            } else {
-            msg.angular.z = 0;
-            publishMsg();
-           }
-           //ROS_INFO("v: %f, w: %f", msg.linear.x, msg.angular.z);
-           loop_rate.sleep();
-        }
-
-    }
-
-    void TurnRight()
-    {
-        ros::Rate loop_rate(10);
-
-        msg.linear.x = 0;
-        msg.linear.y = 0;
-        msg.linear.z = 0;
-        msg.angular.x = 0;
-        msg.angular.y = 0;
-        msg.angular.z = -0.314;
-
-         for (int i = 0; i < 30; i++) {
-
-            if (i < 10) {
-                 msg.angular.z = -1.57;
-                publishMsg();
-             } else {
-                msg.angular.z = 0;
-                publishMsg();
-            }
-            //ROS_INFO("v: %f, w: %f", msg.linear.x, msg.angular.z);
-            loop_rate.sleep();
-          }
-
-    }
-
-    void StraightLeft()
-    {
-       double alpha = 0.2;
-       double diff_distance, angular_vel;
-
-       if (d1 < 8 && d1 > 0 && d3 < 8 && d3 > 0) {
-
-         msg.linear.x = 0.1;
-         msg.angular.z = -0.314;
-
-       } else {
-
-        msg.linear.x = 0.1;
-
-        diff_distance = (double)(d1 - d3);
-        angular_vel = alpha*diff_distance;
-        msg.angular.z = angular_vel;
-       }
-
-       msg.linear.y = 0;
-       msg.linear.z = 0;
-       msg.angular.x = 0;
-       msg.angular.y = 0;
-
-    }
-
-    void StraightRight()
-    {
-       double alpha = -0.2;
-       double diff_distance, angular_vel;
-
-       if (d2 < 8 && d2 > 0 && d4 < 8 && d4 > 0) {
-
-         msg.linear.x = 0.1;
-         msg.angular.z = 0.314;
-
-       } else {
-
-        msg.linear.x = 0.1;
-
-        diff_distance = (double)(d2 - d4);
-        angular_vel = alpha*diff_distance;
-        msg.angular.z = angular_vel;
-       }
-
-       msg.linear.y = 0;
-       msg.linear.z = 0;
-       msg.angular.x = 0;
-       msg.angular.y = 0;
-
+    void MazeCallback(const ras_arduino_msgs::ADConverterConstPtr &msg) {
+        d1 = msg->ch1;
+        d2 = msg->ch2;
+        d3 = msg->ch3;
+        d4 = msg->ch4;
+        d5 = msg->ch5;
     }
 
     void stay() {
@@ -146,21 +42,45 @@ public:
         twist_pub.publish(msg);
     }
 
+    void setClient(int state) {
+        srv.request.state = state;
+        if (state == 1 || state == 2)
+            client = turn_client;
+        else if (state == 3 || state == 4)
+            client = follow_client;
+
+    }
+
+    void callService() {
+        if (client.call(srv)) {
+            ROS_INFO("Succesfully called a service");
+          }
+          else
+          {
+            ROS_ERROR("Failed to call service add_two_ints");
+          }
+    }
+
 private:
+    ros::ServiceClient turn_client;
+    ros::ServiceClient follow_client;
+    ros::ServiceClient client;
+    wall_follower::MakeTurn srv;
+
     geometry_msgs::Twist msg;
 };
 
  int main(int argc, char **argv)
  {
-   ros::init(argc, argv, "maze_follower");
+    ros::init(argc, argv, "maze_follower");
 
-   MazeController mc = MazeController();
+    MazeController mc = MazeController();
 
-   ros::Rate loop_rate(10);
-   ros::Rate rate(1/5);
+    ros::Rate loop_rate(10);
+    ros::Rate rate(1/5);
 
-   int thres_front = 30; //HERE TO CHANGE!
-   int state = 0;
+    int thres_front = 30; //HERE TO CHANGE!
+    int state = 0;
 
    while (ros::ok())
    {
@@ -183,27 +103,14 @@ private:
 
        ROS_INFO("State: %d", state);
 
-       switch(state){
-        case 0:
-           mc.stay();
-           break;
-        case 1:
-             //turn left;
-           mc.TurnLeft();
-           break;
-         case 2:
-             //turn right;
-           mc.TurnRight();
-           break;
-         case 3:
-             //walk straight along the right wall
-           mc.StraightRight();
-           break;
-         case 4:
-             //walk straight along the left wall
-           mc.StraightLeft();
-        }
-       mc.publishMsg();
+       if (state == 0) {
+        mc.stay();
+        mc.publishMsg();
+       } else {
+        mc.setClient(state);
+        mc.callService();
+       }
+
        state = 0;
        if (state == 1 || state == 2) rate.sleep();
        else loop_rate.sleep();
--- src/turn_controller.cpp
+++ src/turn_controller.cpp
@@ -1,60 +1,65 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
 #include "ras_arduino_msgs/ADConverter.h"
+#include <wall_follower/MakeTurn.h>
   
-#include <sstream>
-
 static int distance_sensor_left, distance_sensor_right, distance_sensor_front;
+ros::Publisher twist_pub;
 
-void TurnCallback(const ras_arduino_msgs::ADConverter::ConstPtr &msg)
+/*void TurnCallback(const ras_arduino_msgs::ADConverter::ConstPtr &msg)
  {
    distance_sensor_left = msg->ch1; 
    distance_sensor_right = msg->ch2; 
    distance_sensor_front = msg->ch5;
    ROS_INFO("left: [%d], right: [%d], front: [%d]", distance_sensor_left, distance_sensor_right, distance_sensor_front);
- }
+ }*/
+
+bool turn(wall_follower::MakeTurn::Request &req, wall_follower::MakeTurn::Response &res) {
+
+    geometry_msgs::Twist msg;
+
+    msg.linear.x = 0;
+    msg.linear.y = 0;
+    msg.linear.z = 0;
+    msg.angular.x = 0;
+    msg.angular.y = 0;
+
+    if (req.state == 1) {
+        msg.angular.z = 1.57;
+    } else {
+        msg.angular.z = -1.57;
+    }
+
+    ros::Rate loop_rate(10);
+
+     for (int i = 0; i < 30; i++) {
+
+        if (i < 10) {
+            twist_pub.publish(msg);
+         } else {
+            msg.angular.z = 0;
+            twist_pub.publish(msg);
+        }
+        //ROS_INFO("v: %f, w: %f", msg.linear.x, msg.angular.z);
+        loop_rate.sleep();
+      }
+
+    ROS_INFO("v: %f, w: %f", msg.linear.x, msg.angular.z);
+
+    return true;
+}
 
  int main(int argc, char **argv)
  {
-   ros::init(argc, argv, "turn_controller");
-   ros::NodeHandle n;
+    ros::init(argc, argv, "turn_controller");
+    ros::NodeHandle n;
 
-   ros::Subscriber turn_sub = n.subscribe("kobuki/adc", 1, TurnCallback);
-   //  ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
-   ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("/motor_controller/twist", 1000);
-  
-   thres_front = 10; //HERE TO CHANGE!
-   if (distance_sensor_front > thres_front){
-     if (distance_sensor_left<distance_sensor_right)
-       turn_flag = 1; //left
-     else
-       turn_flag = -1; //right
-   }
-   else{
-  // execute the wall_follower
-}
+    //ros::Subscriber turn_sub = n.subscribe("/ir_sensor_cm", 1, TurnCallback);
+    twist_pub = n.advertise<geometry_msgs::Twist>("/motor_controller/twist", 1);
+    ros::ServiceServer service = n.advertiseService("make_turn", turn);
 
-   ros::Rate loop_rate(10);
-   int count = 0;
-   while (ros::ok() && count<10)
-   {
-     geometry_msgs::Twist msg;
+    ROS_INFO("Ready to make a turn.");
+    ros::spin();
 
-     msg.linear.x = 0.314;
-     msg.linear.y = 0;
-     msg.linear.z = 0;
-     msg.angular.x = 0;
-     msg.angular.y = 0;
-     msg.angular.z = turn_flag * 0.314; //finish the turn in 5 seconds
-  
-     ROS_INFO("v: %f, w: %f", msg.linear.x, msg.angular.z);
-
-     twist_pub.publish(msg);
-  
-     ros::spinOnce(); 
-     loop_rate.sleep();
-     ++count;
-   }
-   return 0;
+    return 0;
  }
